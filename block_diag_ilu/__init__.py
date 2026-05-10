b0006cdcf2b63861b8c5befe876e580698a22b02
Björn Ingvar Dahlgren <bjodah@gmail.com>
2026-05-10 07:09:46 +0200

    drop pkg_resources

--- block_diag_ilu/__init__.py
+++ block_diag_ilu/__init__.py
@@ -3,7 +3,8 @@
 block_diag_ilu is a C++ implementation of an incomplete LU factorization, ILU(0).
 """
 
-from __future__ import (absolute_import, division, print_function)
+from pathlib import Path
+
 from ._release import __version__
 
 from ._block_diag_ilu import (
@@ -15,5 +16,4 @@ from .datastruct import diag_data_len, alloc_compressed, get_compressed
 
 
 def get_include():
-    from pkg_resources import resource_filename, Requirement
-    return resource_filename(Requirement.parse(__name__), '%s/include' % __name__)
+    return str(Path(__file__).parent / "include")
